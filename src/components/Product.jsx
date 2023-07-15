14f5c49b2dbc14d61fcf9067b6fe117af74d6792
Satyanarayan Sen <60086933+Satyanarayan4434@users.noreply.github.com>
2023-07-15 20:34:24 +0530

    NPM

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -5,7 +5,15 @@ function Product({data}) {
 console.log(data);
   return (
     <div>
+      <p>{data.title}</p>
+      <p>{data.description}</p>
       <img src={data.image} alt="" />
+      <div>
+        <p>{data.price}</p>
+        <button>{
+          true? "Add to cart" : "Remove From Cart"
+        }</button>
+      </div>
     </div>
   )
 }
